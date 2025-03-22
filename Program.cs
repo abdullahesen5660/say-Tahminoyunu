cecb9972b47120eb14c8eca7e0e75951e1a91241
Abdullah esen <abdullah.esen5660@gmail.com>
2025-03-22 14:07:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace satıTahminOyunu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                Random rastgele = new Random();
+                int tutulansayi = rastgele.Next(1, 101); // 1 ile 100 arasında sayı tutar
+                int tahmin = 0;
+                int deneme = 0;
+
+                Console.WriteLine("1 ile 100 arasında bir sayı tuttum. Tahmin etmeye çalış!");
+
+                while (tahmin != tutulansayi)
+                {
+                    Console.Write("Tahmininizi girin: ");
+                    tahmin = Convert.ToInt32(Console.ReadLine());
+                    deneme++;
+
+                    if (tahmin < tutulansayi)
+                        Console.WriteLine("Daha büyük bir sayı girin!");
+                    else if (tahmin > tutulansayi)
+                        Console.WriteLine("Daha küçük bir sayı girin!");
+                    else
+                        Console.WriteLine($"Tebrikler! {deneme} denemede doğru tahmin ettiniz.");
+                }
+            }
+        }
+
+    }
+}
+
